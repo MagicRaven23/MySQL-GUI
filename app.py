b6caa279155a4be26f3061a2123757c4da3a7128
MagicRaven23 <facebaby556@gmail.com>
2025-07-30 19:33:40 +0200

    Index

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # Getting input with name = user  in HTML form
+        user = request.form.get("user")
+        # Getting input with name = password in HTML form
+        password = request.form.get("password")
+        return f"mysql -u'{user}' -p'{password}'"
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(port=8080, debug=True)
